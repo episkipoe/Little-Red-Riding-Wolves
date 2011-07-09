f53e8218f95078ddb7a68cee96dcaf23e2e2937a
James Koval <James.Ross.Koval@gmail.com>
2011-07-09 17:26:14 -0500

    turned off expandtabs in vimrc to use tab indent

--- path.h
+++ path.h
@@ -1,12 +1,12 @@
 #include <common/point.h>
 class Path: public Drawable {
-    public:
-        House(Point loc) {
-            location = loc;
-            radius = 10;//TODO:tweak to fit sprite
-        }
+	public:
+		House(Point loc) {
+			location = loc;
+			radius = 10;//TODO:tweak to fit sprite
+		}
 
-        void draw() {
-            draw_texture("path", location, 10);
-        }
+		void draw() {
+			draw_texture("path", location, 10);
+		}
 };
